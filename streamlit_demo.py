23a2ff5c47fc59ae8f6e6e283485325030972c3c
Kphoo-Source <kphoodream10@gmail.com>
2024-09-14 22:31:44 +0630

    Update streamlit_demo.py

--- streamlit_demo.py
+++ streamlit_demo.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
 actor = st.radio(
-  "what's your favourite movie Celebrities",["***Leminho**","***Nay Toe***","***Justic Biber***",captions=["He is a korean acotor","He is a burmese actor.","He is a famours singer"],
-
+  "what's your favourite movie Celebrities",["***Leminho**","***Nay Toe***","***Justic Biber***"],
+  captions=["He is a korean acotor","He is a burmese actor.","He is a famours singer"],
 )
 if actor=="***Leminho**":
   st.write("Same with me")
