cf90657c62e0abecaded79682d1241291393b5fa
Kphoo-Source <kphoodream10@gmail.com>
2024-09-14 22:28:56 +0630

    Create streamlit_demo.py
    Testing

---
+++ streamlit_demo.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+actor = st.radio(
+  "what's your favourite movie Celebrities",["***Leminho**","***Nay Toe***","***Justic Biber***",captions=["He is a korean acotor","He is a burmese actor.","He is a famours singer"],
+
+)
+if actor=="***Leminho**":
+  st.write("Same with me")
+elif actor=="***Nay Toe***":
+  st.write("Why you prefer him")
+else:
+  st.write("he has a wife")
